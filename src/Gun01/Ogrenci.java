37050a7e21bd55e79f225b39768837ee7310fd02
TechnoStudy <ismettechno@gmail.com>
2022-07-06 17:23:11 +0100

    merhaba 4

--- src/Gun01/Ogrenci.java
+++ src/Gun01/Ogrenci.java
@@ -3,9 +3,8 @@ package Gun01;
 public class Ogrenci {
     public static void main(String[] args) {
         System.out.println("merhaba 1");
-
         System.out.println("merhaba 2");
-
         System.out.println("merhaba 3");
+        System.out.println("merhaba 4");
     }
 }
